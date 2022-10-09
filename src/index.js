64d77fe804d7d23503a544d5168f01ffed9b92a7
Yoel Naki <Yoel.Naki@bankleumi.co.il>
2022-10-09 12:21:56 +0300

    fix args

--- src/index.js
+++ src/index.js
@@ -20,7 +20,21 @@ const resetUserData = (id) => {
 
 const client = new Client({
 	authStrategy: new NoAuth(),
+	puppeteer: {
+		args: [
+			'--no-sandbox',
+			'--disable-setuid-sandbox',
+			'--disable-dev-shm-usage',
+			'--disable-accelerated-2d-canvas',
+			'--no-first-run',
+			'--no-zygote',
+			'--single-process', // <- this one doesn't works in Windows
+			'--disable-gpu',
+		],
+		headless: true,
+	},
 });
+
 const sendQuestion = (to, stage) => {
 	client.sendMessage(to, challenges[stage].question);
 };
