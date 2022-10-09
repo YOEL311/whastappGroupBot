13270170d40eec8bce55e091d88723a7477b7e86
Yoel Naki <Yoel.Naki@bankleumi.co.il>
2022-10-09 12:48:40 +0300

    test

--- src/index.js
+++ src/index.js
@@ -22,14 +22,15 @@ const client = new Client({
 	authStrategy: new NoAuth(),
 	puppeteer: {
 		args: [
-			'--no-sandbox',
 			'--disable-setuid-sandbox',
-			'--disable-dev-shm-usage',
-			'--disable-accelerated-2d-canvas',
-			'--no-first-run',
-			'--no-zygote',
-			'--single-process', // <- this one doesn't works in Windows
-			'--disable-gpu',
+			// '--no-sandbox',
+			// '--disable-setuid-sandbox',
+			// '--disable-dev-shm-usage',
+			// '--disable-accelerated-2d-canvas',
+			// '--no-first-run',
+			// '--no-zygote',
+			// '--single-process', // <- this one doesn't works in Windows
+			// '--disable-gpu',
 		],
 		headless: true,
 	},
